043a1c7651ad86dc8ed3b9fdcd7cf5328d14e280
ArchangelD <sdimartino@gmail.com>
2019-01-29 01:02:38 -0800

    Adds jQuery document ready code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,32 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCG9VLV74sbEn8euipo0YIZ8ig9DkQPSFs",
-    authDomain: "train-time-c52c8.firebaseapp.com",
-    databaseURL: "https://train-time-c52c8.firebaseio.com",
-    projectId: "train-time-c52c8",
-    storageBucket: "train-time-c52c8.appspot.com",
-    messagingSenderId: "706775152034"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyCG9VLV74sbEn8euipo0YIZ8ig9DkQPSFs",
+  authDomain: "train-time-c52c8.firebaseapp.com",
+  databaseURL: "https://train-time-c52c8.firebaseio.com",
+  projectId: "train-time-c52c8",
+  storageBucket: "train-time-c52c8.appspot.com",
+  messagingSenderId: "706775152034"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$(document).ready(function() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+});
